2802bd66d9c8edbbd795dc4737664f38f4ea152d
ColeSaunders25 <colesaunders25@marist.com>
2023-11-08 16:25:46 -0500

    added cr climber servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Climber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Climber.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -10,20 +11,24 @@ import org.firstinspires.ftc.teamcode.util.Constants;
 public class Climber implements SubsystemBase{
 
     public DcMotor climber;
+    public CRServo leftClimberServo;
+    public CRServo rightClimberServo;
 
     public static final int RAISE = 0;
     public static final int LOWER = 1;
     public static final int REST = 2;
     public Climber(HardwareMap hwMap){
         climber = hwMap.dcMotor.get("climber");
+        leftClimberServo = hwMap.crservo.get("leftClimberServo");
+        rightClimberServo = hwMap.crservo.get("rightClimberServo");
         climber.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
 
     public void setMode(int mode){
-        if(mode == RAISE)climber.setPower(Constants.ClimberConstants.climberPower);
-        else if(mode == LOWER)climber.setPower(-Constants.ClimberConstants.climberPower);
-        else if(mode == REST)climber.setPower(0);
+        if(mode == RAISE){climber.setPower(Constants.ClimberConstants.climberPower);leftClimberServo.setPower(Constants.ClimberConstants.climberPower);rightClimberServo.setPower(Constants.ClimberConstants.climberPower);}
+        else if(mode == LOWER) {climber.setPower(-Constants.ClimberConstants.climberPower);leftClimberServo.setPower(-Constants.ClimberConstants.climberPower);rightClimberServo.setPower(-Constants.ClimberConstants.climberPower);}
+        else if(mode == REST) {climber.setPower(0);leftClimberServo.setPower(0);rightClimberServo.setPower(0);}
     }
 
     @Override
