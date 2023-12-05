ffdf50cf5fd874fabccaa76e51b3581bc760d997
RussellMarkwalter25 <russellmarkwalter25@marist.com>
2023-12-05 13:46:29 -0500

    Stack Auto and some stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/comp/NoAprilTag/BackdropRedLeftNAT.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.auto.comp.NoAprilTag;
+
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.auto.util.AutoUtil;
+import org.firstinspires.ftc.teamcode.subsystems.BaseRobot;
+import org.firstinspires.ftc.teamcode.util.SampleAuto;
+import org.firstinspires.ftc.teamcode.vision.TeamPropPartitionDetector;
+
+@Autonomous(name="NATRedLeft", group="Backdrop")
+public class BackdropRedLeftNAT extends SampleAuto {
+    BaseRobot robot;
+    int zone;
+    @Override
+    public void onInit() {
+        robot  = new BaseRobot(hardwareMap, AutoUtil.REDLEFTSTART);
+        TeamPropPartitionDetector.startPropDetection(robot.camera, pen);
+    }
+
+    @Override
+    public void onStart() {
+        zone = TeamPropPartitionDetector.getRedPropZone();
+        TeamPropPartitionDetector.endPropDetection();
+        pen.addLine("ZONE: " + zone);
+        pen.update();
+        Actions.runBlocking(robot.autoGenerator.getSpikeAutoAction(AutoUtil.RED, AutoUtil.LEFT, zone));
+        robot.drive.updatePoseEstimate();
+        robot.drive.drawPoseHistory(pen.getPacket().fieldOverlay());
+        pen.addLine("POSE: " + robot.drive.pose.position + " Heading "+ robot.drive.pose.heading);
+        pen.update();
+        Actions.runBlocking(robot.autoGenerator.getBackdropAutoActionNoAprilTag(AutoUtil.RED, AutoUtil.LEFT, zone));
+        robot.drive.updatePoseEstimate();
+        robot.drive.drawPoseHistory(pen.getPacket().fieldOverlay());
+        pen.addLine("POSE: " + robot.drive.pose.position + " Heading "+ robot.drive.pose.heading);
+        pen.update();
+        //Actions.runBlocking(robot.drive.driveToAprilTag(AutoUtil.RED,zone, robot.camera, pen));
+        Actions.runBlocking(robot.outtake());
+        Actions.runBlocking(robot.hopper.hopperOutake());
+        Actions.runBlocking(robot.resetToIntake());
+        robot.drive.updatePoseEstimate();
+        robot.drive.drawPoseHistory(pen.getPacket().fieldOverlay());
+        pen.addLine("POSE: " + robot.drive.pose.position + " Heading "+ robot.drive.pose.heading);
+        pen.update();
+        Actions.runBlocking(robot.autoGenerator.getBackStageParkAutoAction(AutoUtil.RED, AutoUtil.LEFT));
+    }
+
+
+
+
+    @Override
+    public void onStop() {
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/comp/NoAprilTag/BackdropRedRightNAT.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.auto.comp.NoAprilTag;
+
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.auto.util.AutoUtil;
+import org.firstinspires.ftc.teamcode.subsystems.BaseRobot;
+import org.firstinspires.ftc.teamcode.util.SampleAuto;
+import org.firstinspires.ftc.teamcode.vision.TeamPropPartitionDetector;
+
+@Autonomous(name="NATRedRight", group="Backdrop")
+public class BackdropRedRightNAT extends SampleAuto {
+    BaseRobot robot;
+    int zone;
+    @Override
+    public void onInit() {
+        robot  = new BaseRobot(hardwareMap, AutoUtil.REDRIGHTSTART);
+        TeamPropPartitionDetector.startPropDetection(robot.camera, pen);
+    }
+
+    @Override
+    public void onStart() {
+        zone = TeamPropPartitionDetector.getRedPropZone();
+        TeamPropPartitionDetector.endPropDetection();
+        pen.addLine("ZONE: " + zone);
+        pen.update();
+        Actions.runBlocking(robot.autoGenerator.getSpikeAutoAction(AutoUtil.RED, AutoUtil.RIGHT, zone));
+        robot.drive.updatePoseEstimate();
+        robot.drive.drawPoseHistory(pen.getPacket().fieldOverlay());
+        pen.addLine("POSE: " + robot.drive.pose.position + " Heading "+ robot.drive.pose.heading);
+        pen.update();
+        Actions.runBlocking(robot.autoGenerator.getBackdropAutoActionNoAprilTag(AutoUtil.RED, AutoUtil.RIGHT, zone));
+        robot.drive.updatePoseEstimate();
+        robot.drive.drawPoseHistory(pen.getPacket().fieldOverlay());
+        pen.addLine("POSE: " + robot.drive.pose.position + " Heading "+ robot.drive.pose.heading);
+        pen.update();
+        //Actions.runBlocking(robot.drive.driveToAprilTag(AutoUtil.RED,zone, robot.camera, pen));
+        Actions.runBlocking(robot.distanceOuttake());
+        Actions.runBlocking(robot.hopper.hopperOutake());
+        Actions.runBlocking(robot.resetToIntake());
+        robot.drive.updatePoseEstimate();
+        robot.drive.drawPoseHistory(pen.getPacket().fieldOverlay());
+        pen.addLine("POSE: " + robot.drive.pose.position + " Heading "+ robot.drive.pose.heading);
+        pen.update();
+        Actions.runBlocking(robot.autoGenerator.getBackStageParkAutoAction(AutoUtil.RED, AutoUtil.RIGHT));
+    }
+
+
+
+
+    @Override
+    public void onStop() {
+
+    }
+
+}
