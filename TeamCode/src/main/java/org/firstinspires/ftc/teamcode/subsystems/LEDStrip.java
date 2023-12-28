d444c1a1d08f2861d2e7ed3de731baf0ed3e822a
ColeSaunders25 <colesaunders25@marist.com>
2023-12-28 13:18:30 -0600

    REVLedBlinkin added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LEDStrip.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+
+import org.firstinspires.ftc.teamcode.util.ColorfulTelemetry;
+
+public class LEDStrip implements SubsystemBase{
+    RevBlinkinLedDriver strip;
+
+
+    @Override
+    public void printTelemetry(ColorfulTelemetry t) {
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+}
